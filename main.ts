7af716a0ab37a96b2bb07aeb9276db2826863ed2
blazeywazeyalabama <robbinsbl@students.dsbn.org>
2025-09-30 10:06:27 -0400

    This Is The Best Microbit Code Ever.

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
-        # . . # #
-        . # # . .
-        # . . # #
-        . # # . .
-        # . . # #
+        # . # . #
+        . # # . #
+        # . . # .
+        . # # . #
+        # . # . #
         `)
 })
 input.onButtonPressed(Button.AB, function () {
     basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showString("John Here, How Are You?        Idc, Im doing Good In The Motel.")
+    basic.showString("John Here, How Are You?        Idc, Im Doing Good In The Motel.")
 })
